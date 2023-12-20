b3c5e333899c6fffafdc33fb14a298395a5e075c
김성민 <122139342+ksm1025kk@users.noreply.github.com>
2023-12-20 22:46:12 +0900

    [Bronze II] Title: 하얀 칸, Time: 128 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Bronze/1100. 하얀 칸/하얀 칸.java
+++ 백준/Bronze/1100. 하얀 칸/하얀 칸.java
@@ -10,44 +10,16 @@ public class Main {
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		try {
-			String str1[][] = new String[8][8];
-			String str2[][] = new String[8][8];
-			int count = 8;
-			int num1 = 0;
-			while (count != 0) {
-				String str[] = br.readLine().split("");
-				int num2 = 0;
-				for (int i = 0; i < 8; i++) {
-					str1[num1][num2] = str[num2];
-					num2++;
-				}
-				num1++;
-				count--;
-			}
-
-			for (int i = 0; i < 8; i++) {
-				for (int j = 0; j < 8; j++) {
-					if (i % 2 == 0) {
-						if (j % 2 == 0) {
-							str2[i][j] = "F";
-						}
-					} else {
-						if (j % 2 != 0) {
-							str2[i][j] = "F";
-						}
+			int count = 0;
+			for(int i=0; i<8; i++) {
+				String str = br.readLine();
+				for(int j=0; j<8; j++) {
+					if((i+j)%2 == 0 && str.charAt(j)=='F') {
+						count++;
 					}
 				}
 			}
-
-			int w = 0;
-			for (int i = 0; i < 8; i++) {
-				for (int j = 0; j < 8; j++) {
-					if (str1[i][j].equals(str2[i][j])) {
-						w++;
-					}
-				}
-			}
-			bw.write("" + w);
+			bw.write("" + count);
 			bw.flush();
 			br.close();
 		} catch (Exception e) {
