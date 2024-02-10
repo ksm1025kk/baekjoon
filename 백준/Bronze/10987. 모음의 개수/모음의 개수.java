a8d261f2a8190fe6e5a49a93c5016924437f4514
김성민 <122139342+ksm1025kk@users.noreply.github.com>
2024-02-10 13:30:53 +0900

    [Bronze IV] Title: 모음의 개수, Time: 140 ms, Memory: 15836 KB -BaekjoonHub

---
+++ 백준/Bronze/10987. 모음의 개수/모음의 개수.java
@@ -0,0 +1,25 @@
+
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.HashSet;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int count = 0;
+        String s = br.readLine();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') { 
+            	count++;
+            }
+        }
+        bw.write(count + " ");
+		bw.flush();
+		br.close();
+	}
+}
