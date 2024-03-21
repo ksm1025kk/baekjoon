8c44544549137b2f665779754f11b0f213902cfd
김성민 <122139342+ksm1025kk@users.noreply.github.com>
2024-03-21 22:35:58 +0900

    [Bronze IV] Title: Identifying tea, Time: 132 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Bronze/11549. Identifying tea/Identifying tea.java
@@ -0,0 +1,28 @@
+
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.math.BigInteger;
+
+public class Main {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		String a = br.readLine();
+		String str[] = br.readLine().split(" ");
+		int num = 0;
+		for(String n:str) {
+			if(n.equals(a)) {
+				num++;
+			}
+		}
+		bw.write(num + "");
+		bw.flush();
+		br.close();
+	}
+}
+
+		
