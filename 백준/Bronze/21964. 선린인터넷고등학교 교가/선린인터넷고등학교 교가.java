dd4077375a0430892b6868c6e2f42a5491179787
김성민 <122139342+ksm1025kk@users.noreply.github.com>
2024-03-30 20:07:21 +0900

    [Bronze IV] Title: 선린인터넷고등학교 교가, Time: 136 ms, Memory: 15408 KB -BaekjoonHub

---
+++ 백준/Bronze/21964. 선린인터넷고등학교 교가/선린인터넷고등학교 교가.java
@@ -0,0 +1,22 @@
+
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.math.BigInteger;
+
+public class Main {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int count = Integer.parseInt(br.readLine());
+		String str = br.readLine();
+		bw.write(str.substring(count-5));
+		bw.flush();
+		br.close();
+	}
+}
+
+		
