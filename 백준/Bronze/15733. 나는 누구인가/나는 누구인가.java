1a17b0cd1a2c9367a433f0128b48f09d742bcd10
김성민 <122139342+ksm1025kk@users.noreply.github.com>
2024-08-23 22:04:24 +0900

    [Bronze V] Title: 나는 누구인가, Time: 92 ms, Memory: 13932 KB -BaekjoonHub

---
+++ 백준/Bronze/15733. 나는 누구인가/나는 누구인가.java
@@ -0,0 +1,9 @@
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write("I'm Sexy");
+        bw.flush();
+    }
+}
